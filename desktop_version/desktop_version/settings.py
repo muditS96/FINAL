7335feba93c1306c48fce8a4e02dd38b584754c4
muditS96 <MUDITKA9044@GMAIL.COM>
2022-06-19 16:31:46 +0530

    updated

--- desktop_version/desktop_version/settings.py
+++ desktop_version/desktop_version/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ovx(qzt*25@47y^njkur#xkf0*)x*$r*!42olr(y9fi9e7f#ep'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["alpha-bit8726.herokuapp.com"]
 
 
 # Application definition
@@ -85,9 +85,9 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-# import dj_database_url
-# dj_from_env=dj_database.url.config(conn_max_age=600)
-# DATABASES['default'].update(dj_from_env)
+import dj_database_url
+dj_from_env=dj_database.url.config(conn_max_age=600)
+DATABASES['default'].update(dj_from_env)
 
 
 
