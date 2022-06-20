a1d02a49f74526cf29ad91456e30f255e5df9642
muditS96 <MUDITKA9044@GMAIL.COM>
2022-06-20 16:00:10 +0530

    added models also

--- desktop_version/webReg/models.py
+++ desktop_version/webReg/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+#    previuosly used as model 
+    # name:str
+    # img:str
+    # desc:str
+    # price:int
+    # offer:bool
+# now being used as model for migration
+    name=models.CharField(max_length=100)
+    img=models.ImageField( upload_to=None)
+    desc=models.TextField()
+    price=models.IntegerField()
+    offer=models.BooleanField(default=False)
+
+# Create your models here.
